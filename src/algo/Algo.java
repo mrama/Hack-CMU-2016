afe6204a29de4dee66af432db1e6c4d32bc7b7a4
azdavis <ariel.z.davis@icloud.com>
2016-09-17 05:12:34 -0400

    darken the poster

--- src/algo/Algo.java
+++ src/algo/Algo.java
@@ -11,6 +11,7 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Scalar;
+import java.awt.image.BufferedImage;
 
 /** @class Algo
  *  @author Pavel Khokhlov <pkhokhlo@andrew.cmu.edu>
@@ -19,7 +20,7 @@ import org.opencv.core.Scalar;
  */
 public class Algo
 {
-	public static void algo(int[][] wallCoords)
+	public static BufferedImage algo(int[][] wallCoords)
 	{
 		// Load OpenCV
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -45,6 +46,13 @@ public class Algo
 		MatOfPoint2f wallPoints = new MatOfPoint2f(
 			new Point(wallCoords[0][0], wallCoords[0][1]),
 			new Point(wallCoords[1][0], wallCoords[1][1]),
+			new Point(wallCoords[3][0], wallCoords[3][1]),
+			new Point(wallCoords[2][0], wallCoords[2][1])
+		);
+
+		MatOfPoint wallPointsPoly = new MatOfPoint(
+			new Point(wallCoords[0][0], wallCoords[0][1]),
+			new Point(wallCoords[1][0], wallCoords[1][1]),
 			new Point(wallCoords[2][0], wallCoords[2][1]),
 			new Point(wallCoords[3][0], wallCoords[3][1])
 		);
@@ -57,9 +65,26 @@ public class Algo
 		Imgproc.warpPerspective(posterImg, tmpImg, homog, tmpImg.size());
 		// Create a resulting image matrix
 		Mat resImg = new Mat();
+		Core.fillConvexPoly(wallImg, wallPointsPoly, new Scalar(0));
 		// Add the warped picture to the wall
 		Core.add(wallImg, tmpImg, resImg);
-		// Writes resulting image
+		// Writes resulting image TODO remove
 		Highgui.imwrite("src/img/result.jpg", resImg, new MatOfInt(95));
+
+		// http://stackoverflow.com/questions/27086120/convert-opencv-mat-
+		// object-to-bufferedimage
+		Imgproc.cvtColor(resImg, resImg, Imgproc.COLOR_RGB2GRAY, 0);
+
+		// Create an empty image in matching format
+		BufferedImage ret = new BufferedImage(
+			resImg.width(),
+			resImg.height(),
+			BufferedImage.TYPE_BYTE_GRAY
+		);
+/*
+		byte[] data
+			= ((DataBufferByte) ret.getRaster().getDataBuffer()).getData();
+		resImg.get(0, 0, data);*/
+		return ret;
 	}
 }
