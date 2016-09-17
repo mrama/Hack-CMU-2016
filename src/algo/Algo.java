eb4415613c3b445c55ae76ba9bd91f5c49f385a9
pkhokhlov <pkhokhlov@hotmail.com>
2016-09-17 01:47:45 -0400

    added OpenCV implementation of homography algo

---
+++ src/algo/Algo.java
@@ -0,0 +1,63 @@
+package src.algo;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Size;
+import org.opencv.core.Point;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.Scalar;
+
+/** @class Algo
+ *  @author Pavel Khokhlov <pkhokhlo@andrew.cmu.edu>
+ *  @author Ariel Davis <azdavis@andrew.cmu.edu>
+ *  @thanks https://github.com/spmallick/learnopencv/tree/master/Homography
+ */
+public class Algo
+{
+    public static void algo()
+    {
+        // Load OpenCV
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        // Poster image
+        Mat posterImg = Highgui.imread("src/img/poster/02.jpg");
+        Size posterSize = posterImg.size();
+
+        // Border of the poster, used for the point correspondence
+        // top left, top right, bottom left, bottom right
+        MatOfPoint2f posterPoints = new MatOfPoint2f(
+            new Point(0, 0),
+            new Point(posterSize.width - 1, 0),
+            new Point(0, posterSize.height - 1 ),
+            new Point(posterSize.width - 1, posterSize.height -1)
+        );
+
+        // Destination image
+        Mat wallImg = Highgui.imread("src/img/wall.jpg");
+
+        // Points on the wall where we map the poster
+        // top left, top right, bottom left, bottom right
+        MatOfPoint2f wallPoints = new MatOfPoint2f(
+            new Point(1457, 1118),
+            new Point(2451, 1302),
+            new Point(1457, 2213),
+            new Point(2451, 2039)
+        );
+
+        // Clone the wall
+        Mat tmpImg = wallImg.clone();
+        // Calculate homography between source and destination points
+        Mat homog = Calib3d.findHomography(posterPoints, wallPoints);
+        // Warp poster using the homography
+        Imgproc.warpPerspective(posterImg, tmpImg, homog, wallImg.size());
+        // Add the warped picture to the wall
+        Core.add(wallImg, tmpImg, wallImg);
+        // Writes resulting image
+        Highgui.imwrite("src/img/result.jpg", wallImg, new MatOfInt(95));
+    }
+}
