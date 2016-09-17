6c716adac7c273e42b5404ee2cc5ff592b40ce6e
mrama <rama.mannava@yahoo.com>
2016-09-17 15:34:17 -0400

    Made posters display final image + buttons

--- src/gui/Posters.java
+++ src/gui/Posters.java
@@ -2,6 +2,7 @@ package src.gui;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.EventQueue;
 import java.awt.image.BufferedImage;
 import java.awt.Image;
@@ -18,50 +19,74 @@ import javax.swing.JPanel;
 
 import src.Main;
 
-public class Posters extends JFrame
+public class Posters extends JFrame implements ActionListener
 {
 	private JPanel contentPane;
-	private JButton btnOneMore, btnExit;
+	private JButton btnOneMore, btnStartOver, btnExit;
 
 	public Posters(BufferedImage bi)
 	{
-		double scaleFactor = 0.1;
+		// double scaleFactor = 0.25;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1420, 1080);
+		double scaleFactor = 0.75 * (bi.getHeight() < bi.getWidth() ? (1.0*this.getWidth())/bi.getWidth() : (1.0*this.getHeight())/bi.getHeight());
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		System.out.printf("OH FUCK %b\n", bi == null);
-		System.out.println(bi);
 		Image image = bi.getScaledInstance(
-				(int) (4640 * scaleFactor),
-				(int) (3480 * scaleFactor),
+				(int) (4640 * 0.75*scaleFactor),
+				(int) (3480 * 0.75*scaleFactor),
 					Image.SCALE_SMOOTH
 				);
-		System.out.println(image);
 		JLabel lblNewLabel = new JLabel(new ImageIcon(image));
 		lblNewLabel.setBounds(
-			0,
-			0,
+			90,
+			-25,
 			(int) (4640 * scaleFactor),
 			(int) (3480 * scaleFactor)
 		);
 		contentPane.add(lblNewLabel);
 
-		JButton btnOneMore = new JButton("One More!");
-		btnOneMore.setBounds(30, 923, 171, 41);
+		btnOneMore = new JButton("One More!");
+		btnOneMore.addActionListener(this);
+		btnOneMore.setBounds(
+			this.getWidth() / 8,
+			this.getHeight() - 400,
+			171,
+			41
+		);
 		contentPane.add(btnOneMore);
 
-		JButton btnExit = new JButton("Exit");
-		btnExit.setBounds(1173, 923, 171, 41);
+		btnStartOver = new JButton("Start Over");
+		btnStartOver.addActionListener(this);
+		btnStartOver.setBounds(
+			3 * this.getWidth() / 8,
+			this.getHeight() - 400,
+			171,
+			41
+		);
+		contentPane.add(btnStartOver);
+
+		btnExit = new JButton("Exit");
+		btnExit.addActionListener(this);
+		btnExit.setBounds(
+			5 * this.getWidth() / 8,
+			this.getHeight() - 400,
+			171,
+			41
+		);
 		contentPane.add(btnExit);
 	}
 
-	public void actionPerformed(ActionEvent e)
-	{
-		if (e.getSource() == btnOneMore)
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnOneMore)
+		{
+			// TODO
+		}
+		else if(e.getSource() == btnStartOver)
 		{
 			Main.main(new String[] {});
 			this.dispose();
@@ -71,5 +96,4 @@ public class Posters extends JFrame
 			System.exit(0);
 		}
 	}
-
 }
