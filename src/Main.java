9db715ef13b6ba6fb4b37deb4da0521c7932499b
azdavis <ariel.z.davis@icloud.com>
2016-09-17 04:19:07 -0400

    remove getcwd

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,6 @@ public class Main
 
 	public static void main(String[] args) throws IOException
 	{
-		System.out.println(System.getProperty("user.dir"));
 		Menu menu = new Menu();
 		menu.setVisible(true);
 		Dots dots = new Dots();
