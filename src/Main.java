47688ca6b7d95e993f51cf9842f3b3fc4282df88
azdavis <ariel.z.davis@icloud.com>
2016-09-17 03:48:28 -0400

    whitespace

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,31 @@ import src.gui.Menu;
 
 public class Main
 {
-    private static double height, width;
-    private static int[][] dotCoords;
-    private static boolean multiple;
+	private static double height, width;
+	private static int[][] dotCoords;
+	private static boolean multiple;
 
-    public static void updateMenuVars(
-        double height,
-        double width,
-        boolean multiple
-    ) {
-        Main.height = height;
-        Main.width = width;
-        Main.multiple = multiple;
-    }
+	public static void updateMenuVars(
+		double height,
+		double width,
+		boolean multiple
+	) {
+		Main.height = height;
+		Main.width = width;
+		Main.multiple = multiple;
+	}
 
-    public static void updateDotsVars(int[][] dotCoords)
-    {
-        Main.dotCoords = dotCoords;
-    }
+	public static void updateDotsVars(int[][] dotCoords)
+	{
+		Main.dotCoords = dotCoords;
+	}
 
-    public static void main(String[] args) throws IOException
-    {
-        System.out.println(System.getProperty("user.dir"));
-        Menu menu = new Menu();
-        menu.setVisible(true);
-        Dots dots = new Dots();
-        dots.setVisible(true);
-    }
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println(System.getProperty("user.dir"));
+		Menu menu = new Menu();
+		menu.setVisible(true);
+		Dots dots = new Dots();
+		dots.setVisible(true);
+	}
 }
--- src/algo/Algo.java
+++ src/algo/Algo.java
@@ -19,45 +19,45 @@ import org.opencv.core.Scalar;
  */
 public class Algo
 {
-    public static void algo(int[][] wallCoords)
-    {
-        // Load OpenCV
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	public static void algo(int[][] wallCoords)
+	{
+		// Load OpenCV
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        // Poster image
-        Mat posterImg = Highgui.imread("src/img/poster/02.jpg");
-        Size posterSize = posterImg.size();
+		// Poster image
+		Mat posterImg = Highgui.imread("src/img/poster/02.jpg");
+		Size posterSize = posterImg.size();
 
-        // Border of the poster, used for the point correspondence
-        // top left, top right, bottom left, bottom right
-        MatOfPoint2f posterPoints = new MatOfPoint2f(
-            new Point(0, 0),
-            new Point(posterSize.width - 1, 0),
-            new Point(0, posterSize.height - 1 ),
-            new Point(posterSize.width - 1, posterSize.height -1)
-        );
+		// Border of the poster, used for the point correspondence
+		// top left, top right, bottom left, bottom right
+		MatOfPoint2f posterPoints = new MatOfPoint2f(
+			new Point(0, 0),
+			new Point(posterSize.width - 1, 0),
+			new Point(0, posterSize.height - 1 ),
+			new Point(posterSize.width - 1, posterSize.height -1)
+		);
 
-        // Destination image
-        Mat wallImg = Highgui.imread("src/img/wall.jpg");
+		// Destination image
+		Mat wallImg = Highgui.imread("src/img/wall.jpg");
 
-        // Points on the wall where we map the poster
-        // top left, top right, bottom left, bottom right
-        MatOfPoint2f wallPoints = new MatOfPoint2f(
-            new Point(wallCoords[0][0], wallCoords[0][1]),
-            new Point(wallCoords[1][0], wallCoords[1][1]),
-            new Point(wallCoords[2][0], wallCoords[2][1]),
-            new Point(wallCoords[3][0], wallCoords[3][1])
-        );
+		// Points on the wall where we map the poster
+		// top left, top right, bottom left, bottom right
+		MatOfPoint2f wallPoints = new MatOfPoint2f(
+			new Point(wallCoords[0][0], wallCoords[0][1]),
+			new Point(wallCoords[1][0], wallCoords[1][1]),
+			new Point(wallCoords[2][0], wallCoords[2][1]),
+			new Point(wallCoords[3][0], wallCoords[3][1])
+		);
 
-        // Clone the wall
-        Mat tmpImg = wallImg.clone();
-        // Calculate homography between source and destination points
-        Mat homog = Calib3d.findHomography(posterPoints, wallPoints);
-        // Warp poster using the homography
-        Imgproc.warpPerspective(posterImg, tmpImg, homog, wallImg.size());
-        // Add the warped picture to the wall
-        Core.add(wallImg, tmpImg, wallImg);
-        // Writes resulting image
-        Highgui.imwrite("src/img/result.jpg", wallImg, new MatOfInt(95));
-    }
+		// Clone the wall
+		Mat tmpImg = wallImg.clone();
+		// Calculate homography between source and destination points
+		Mat homog = Calib3d.findHomography(posterPoints, wallPoints);
+		// Warp poster using the homography
+		Imgproc.warpPerspective(posterImg, tmpImg, homog, wallImg.size());
+		// Add the warped picture to the wall
+		Core.add(wallImg, tmpImg, wallImg);
+		// Writes resulting image
+		Highgui.imwrite("src/img/result.jpg", wallImg, new MatOfInt(95));
+	}
 }
